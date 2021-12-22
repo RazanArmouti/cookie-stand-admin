6a9ca2c65836948e05f0a5d7078a11ff51daa830
Razan Armouti <razanarmote@yahoo.com>
2021-12-22 20:51:09 +0200

    v3

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import Head from 'next/head'
+import { useState } from 'react'
+import { useAuth } from '../contexts/auth'
+import useResource from '../hooks/useResource'
+import CookieStandAdmin from "../components/CookieStandAdmin"
+import LoginPage from './Login'
+export default function Home() {
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const { user, login, logout } = useAuth();
+  const { resources, loading, createResource, deleteResource } = useResource();
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <Head>
+        <title>Cookie Stand Admin</title>
+      </Head>
+      {user ? <CookieStandAdmin
+        cookiesArray ={resources}
+        createResource = {createResource}
+        logout = {logout}
+        deleteResource={deleteResource}
+        
+      /> : <LoginPage login={login} user={user} />
+      }
+
+    </div>
+  )
 }
-
-export default MyApp
